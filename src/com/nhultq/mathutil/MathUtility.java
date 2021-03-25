cae033a40c76ea750baea4e79c1606a7109922d2
NhuLTQSE140703 <nhultqse140703@fpt.edu.vn>
2021-03-25 16:28:38 +0700

    Update RRECURSION

--- src/com/nhultq/mathutil/MathUtility.java
+++ src/com/nhultq/mathutil/MathUtility.java
@@ -23,14 +23,20 @@ public class MathUtility {
         if(n<0 || n>20){
             throw new IllegalArgumentException("n must be from 0 to 20");
         }
+        
+        //sửa code hàm giai thừa, dùng đệ quy 
+        //đệ quy: vòng lại chính mình với một quy mô khác
+        //recursion, làm khéo nếu không nó đi mãi mãi
+        //stackoverflow.com
         if(n==0 || n ==1){
             return 1; //0!, 1! = 1
         }
+        return n * getFactorial(n-1); //n! = n * (n-1)!
         
-        long result = 1; //XANH LOCAL VÀ SERVER //giả bộ
-        for(int i = 2; i<=n; i++){
-            result *= i;
-        }
-        return result; //nhân từ 2, 3, 4, 5, 6...
+//        long result = 1; //XANH LOCAL VÀ SERVER //giả bộ
+//        for(int i = 2; i<=n; i++){
+//            result *= i;
+//        }
+//        return result; //nhân từ 2, 3, 4, 5, 6...
     }
 }
